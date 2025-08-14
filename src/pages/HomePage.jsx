e978d2a99b977478414b2cc061114752b18b0d6e
Carlos Arias <carlos.ariasalmanzar@gmail.com>
2025-08-13 22:06:48 -0400

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+
+export const HomePage = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ThemeToggle />
+      <StarBackground />
+    </div>
+  );
+};
